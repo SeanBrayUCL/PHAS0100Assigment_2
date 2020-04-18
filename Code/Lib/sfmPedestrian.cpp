515e345bd50157ec453785e6fdbbf333c7e4715b
Sean Bray <ucapsb5@ucl.ac.uk>
2020-04-18 21:30:09 +0100

    Adding implementation file for Pedestrian class and adding to CMake list

---
+++ Code/Lib/sfmPedestrian.cpp
@@ -0,0 +1,25 @@
+#include "sfmBasicTypes.h"
+#include "sfmPedestrian.h"
+
+namespace sfm {
+
+ 
+// Date constructor
+Pedestrian::Pedestrian(sfm::pos2d origin, sfm::pos2d destination, sfm::dir2d velocity, sfm::pos2d position, double desired_speed, double relaxation_time)
+{
+    SetPedestrian(origin, destination, velocity, position, desired_speed, relaxation_time);
+}
+ 
+// Date member function
+void Pedestrian::SetPedestrian(sfm::pos2d origin, sfm::pos2d destination, sfm::dir2d velocity, sfm::pos2d position, double desired_speed, double relaxation_time)
+{
+    m_origin = origin;
+    m_destination = destination;
+    m_velocity = velocity;
+    m_position = position;
+    m_desired_speed = desired_speed;
+    m_relaxation_time = relaxation_time;
+
+}
+
+}
