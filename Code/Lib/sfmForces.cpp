5ac750e161044eefe298e719c8d1515aa5ca9597
Sean Bray <ucapsb5@ucl.ac.uk>
2020-04-18 23:54:41 +0100

    Adding function to calculate ellipse seminior axis

--- Code/Lib/sfmForces.cpp
+++ Code/Lib/sfmForces.cpp
@@ -2,6 +2,9 @@
 #include "sfmForces.h"
 #include <cmath>
 
+const double dt = 2;
+
+
 namespace sfm 
 {
 
@@ -22,6 +25,24 @@ sfm::dir2d attractive_force_to_destination(Pedestrian pedestrian)
     sfm::dir2d actual_velocity = pedestrian.getvelocity();
     double relaxation_time = pedestrian.getrelaxation_time();
     return (desired_velocity - actual_velocity)*(1/relaxation_time);
+}
+
+
+double semiminor_axis_ellipse(Pedestrian pedestrian_1, Pedestrian pedestrian_2)
+{
+    sfm::pos2d position__pedestrian_1 = pedestrian_1.getposition();
+    sfm::pos2d position_pedestrian_2 = pedestrian_2.getposition();
+    sfm::dir2d direction_vector = position_pedestrian_2.direction(position__pedestrian_1);
+    double term_1 = sqrt(direction_vector.scalar_product(direction_vector));
+
+    sfm::dir2d desired_direction_pedestrian_2 = desired_direction_unit_vector(pedestrian_2);
+    double desired_speed_pedestrian_2 = pedestrian_2.getdesired_speed();
+    sfm::dir2d velocity_pedestrian_2 = desired_direction_pedestrian_2*desired_speed_pedestrian_2*dt;
+    sfm::dir2d direction_vector_less_velocity = (direction_vector - velocity_pedestrian_2);
+    double term_2 = sqrt(direction_vector_less_velocity.scalar_product(direction_vector_less_velocity));
+
+    double term_3 = desired_speed_pedestrian_2*dt;
+    return sqrt(pow((term_1 +term_2),2)-pow(term_3,2));
 }
 
 }
--- Code/Lib/sfmForces.h
+++ Code/Lib/sfmForces.h
@@ -4,6 +4,7 @@
 #include "sfmBasicTypes.h"
 #include "sfmPedestrian.h"
 
+
 namespace sfm 
 {
 
@@ -11,6 +12,8 @@ sfm::dir2d desired_direction_unit_vector(Pedestrian pedestrian);
 
 // force 1 
 sfm::dir2d attractive_force_to_destination(Pedestrian pedestrian);
+
+double semiminor_axis_ellipse(Pedestrian pedestrian_1, Pedestrian pedestrian_2);
 
 }
 #endif
