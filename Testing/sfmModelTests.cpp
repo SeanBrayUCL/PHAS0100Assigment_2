fceebf5cad3298dd68af0b7cd1abfdc8d447b4f6
Sean Bray <ucapsb5@ucl.ac.uk>
2020-04-20 23:55:28 +0100

    Adding testing for ped_ped_repulsive_force

--- Testing/sfmModelTests.cpp
+++ Testing/sfmModelTests.cpp
@@ -10,7 +10,7 @@ sfm::pos2d origin (0,0);
 sfm::pos2d destination(10,10); 
 sfm::dir2d velocity(1,1); 
 sfm::pos2d position(5,5); 
-double desired_speed = 2; 
+double desired_speed = 1; 
 double relaxation_time = 1;
 
 Pedestrian ped(origin, destination, velocity, position, desired_speed, relaxation_time);
@@ -49,10 +49,6 @@ sfm::pos2d one_d_destination(1,0);
 sfm::pos2d one_d_position(0,0); 
 Pedestrian one_d_pedestrian(origin, one_d_destination, zero_velocity, one_d_position, unit_speed, relaxation_time);
 
-
-
-
-
 TEST_CASE("Test attractive_force_to_destination", "[Tests]") {
     // position on x/y_wrap should be same as (0,0)
     REQUIRE(sfm::attractive_force_to_destination(stationary_pedestrian)[0] == 0);
@@ -61,9 +57,30 @@ TEST_CASE("Test attractive_force_to_destination", "[Tests]") {
     REQUIRE(sfm::attractive_force_to_destination(unit_desired_speed_pedestrian)[1] == sfm::desired_direction_unit_vector(unit_desired_speed_pedestrian)[1]);    
     REQUIRE(sfm::attractive_force_to_destination(one_d_pedestrian)[1] == 1);
     REQUIRE(sfm::attractive_force_to_destination(one_d_pedestrian)[0] == 0);
-
 }
 
+//testing repulsive ped_ped_repulsive_force
+//create three pedestrian (1,2,3), existing in 1 dimnsion (x direction)
+sfm::pos2d destination_1(2,0); 
+sfm::dir2d velocity_1(1,0); 
+sfm::pos2d position_1(1,0); 
+sfm::pos2d destination_2(10,0); 
+sfm::dir2d velocity_2(1,0); 
+sfm::pos2d position_2(1.1,0); 
+sfm::pos2d destination_3(9,0); 
+sfm::dir2d velocity_3(1,0); 
+sfm::pos2d position_3(8,0); 
+
+Pedestrian pedestrian_1(origin, destination_1, velocity_1, position_1, desired_speed, relaxation_time);
+Pedestrian pedestrian_2(origin, destination_2, velocity_2, position_2, desired_speed, relaxation_time);
+Pedestrian pedestrian_3(origin, destination_3, velocity_3, position_3, desired_speed, relaxation_time);
+
+TEST_CASE("Test ped_ped_repulsive_force", "[Tests]") {
+    // position on x/y_wrap should be same as (0,0)
+    REQUIRE(sfm::ped_ped_repulsive_force(pedestrian_1,pedestrian_2)[0] == 0); //no force in y direction
+    REQUIRE(sfm::ped_ped_repulsive_force(pedestrian_1,pedestrian_2)[1] <= 0); // force should be repulsive
+    REQUIRE(sfm::ped_ped_repulsive_force(pedestrian_1,pedestrian_2)[1] < sfm::ped_ped_repulsive_force(pedestrian_1,pedestrian_3)[1]); //pedestrian_3 is further away so expect a weaker force
+}
 
 
 
